94817a21bc18cda8117f9a1d5e9c9e9cf57ae7e2
MasterGKJ <kjgao1210@gmail.com>
2017-04-18 19:33:30 +0800

    Create abc.h

---
+++ abc.h
@@ -0,0 +1,34 @@
+#ifndef ABC_H
+#define ABC_H
+
+#define TEST_H
+
+#include <QtCore/QCoreApplication>
+#include <QCoreApplication>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <fstream>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/ml/ml.hpp>
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <memory>
+#include <caffe/caffe.hpp>
+#ifdef USE_OPENCV
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#endif  // USE_OPENCV
+#include <algorithm>
+#include <iomanip>
+#include <iosfwd>
+#include <utility>
+#include <vector>
+
+#include<QDebug>
+#include<iostream>
+
+#include <detector.h>
+
+#endif // ABC_H
